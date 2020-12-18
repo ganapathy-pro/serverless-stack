6720fa2c266419410db9586443e529fe840525ac
Ganapathy Elumalai <ganapathy@trypcloud.com>
2020-12-18 11:32:57 +0400

    Adding a billing API

--- create.js
+++ create.js
@@ -8,8 +8,8 @@ export const main = handler(async(event, context) => {
         TableName: process.env.tableName,
         Item: {
             // The attributes of the item to be created
-            userId: "123", // The id of the author
-            noteId: uuid.v1(), // A unique uuid
+            userid: event.requestContext.identity.cognitoIdentityId, // The id of the author
+            noteid: uuid.v1(), // A unique uuid
             content: data.content, // Parsed from request body
             attachment: data.attachment, // Parsed from request body
             createdAt: Date.now(), // Current Unix timestamp
